f9a8c2ae0c44e44268fd01260f5c3b515bcc65ed
GavinKoli <gavinkoli2000@gmail.com>
2021-10-15 13:40:28 +0530

    1

--- app/src/main/java/com/example/beproject/Navigationdrawer.java
+++ app/src/main/java/com/example/beproject/Navigationdrawer.java
@@ -3,30 +3,27 @@ package com.example.beproject;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.beproject.databinding.ActivityNavigationdrawerBinding;
 
 public class Navigationdrawer extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityNavigationdrawerBinding binding;
+private ActivityNavigationdrawerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityNavigationdrawerBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+     binding = ActivityNavigationdrawerBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNavigationdrawer.toolbar);
         binding.appBarNavigationdrawer.fab.setOnClickListener(new View.OnClickListener() {
