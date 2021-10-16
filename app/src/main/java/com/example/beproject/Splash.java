8dc628a9c70c3b41813113118098b501243285d9
GavinKoli <gavinkoli2000@gmail.com>
2021-10-16 19:02:18 +0530

    1

--- app/src/main/java/com/example/beproject/Splash.java
+++ app/src/main/java/com/example/beproject/Splash.java
@@ -1,14 +1,32 @@
 package com.example.beproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Handler;
+import android.content.Intent;
 
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_Splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intend = new Intent(Splash.this , Home.class);
+                startActivity(intend);
+            }
+        },delayMillis: 3000);
+
+
+
     }
 }
